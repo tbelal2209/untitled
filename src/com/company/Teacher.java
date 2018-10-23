bc245d2e59e3191edfba451adf2f2a12d4deb29f
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-23 19:06:14 -0400

    Update Teacher.java

--- src/com/company/Teacher.java
+++ src/com/company/Teacher.java
@@ -1,4 +1,35 @@
 package com.company;
 
-public class Teacher {
+public abstract class Teacher {
+    private String Subject;
+    private String Title;
+    public Teacher(double GPA)
+    {
+        this.Subject= Subject;
+    }
+    public abstract String toString();
+    public String getSubject()
+    {
+        return Subject;
+    }
+    public void setSubject(String Subject)
+    {
+        this.Subject = Subject;
+    }
+    public String getTitle() {
+        return Title;
+    }
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+
+    public boolean equals(Teacher teacher){
+        if(this.Subject.equals(teacher.getSubject())&& this.Title.equals(teacher.getTitle()))
+        {
+            return true;
+        }
+        else return false;
+    }
 }
+
