0e6f2622c68b7edb266e7b1a8b4218a5463eac78
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-23 19:07:10 -0400

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class Runner {
+    public static String[] firstNames = {"Jacky","Runnie","Zhi"};
+    public static String[] familyNames = {"Thunder","Sky","Brick"};
+    public static void main (String [] arge)
+    {
+        Student[] students = new Student[34];
+        for (int i = 0; i<34; i++)
+        {
+            students[i] = randomStudent();
+        }
+        Teacher teach = new Teacher("Folwell", "Nathan","Mr.", "CSA");
+        Classroom ourClass = new Classroom(students,teach);
+        ourClass.printClass();
+    }
 }
