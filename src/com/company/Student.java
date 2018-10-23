79cd52ac6decd3bd4c5bbc207ee61615ea04b668
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-23 19:06:43 -0400

    Update Student.java

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,9 +1,26 @@
 package com.company;
 
-public class Student {
+public abstract class Student {
     private double GPA;
     public Student(double GPA)
     {
         this.GPA= GPA;
+    }
+    public abstract String toString();
+    public double getGPA()
+    {
+        return GPA;
+    }
+    public void setGPA(double GPA)
+    {
+        this.GPA = GPA;
+    }
+
+    public boolean equals(Student student){
+        if(this.GPA == (student.getGPA()))
+        {
+            return true;
+        }
+        else return false;
     }
 }
