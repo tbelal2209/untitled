dfc86a75bb71e53edc8e8d8aa77f2aea1981d7e0
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-26 08:43:24 -0400

    Update Classroom.java

--- src/com/company/Classroom.java
+++ src/com/company/Classroom.java
@@ -1,12 +1,39 @@
 package com.company;
 
 public abstract class Classroom {
-private String Subject; 
-private int Students;
-}
-public class (Subject , Students){
-  this. Subject = Subject ;
-  this. Students = Students;
+    private String Subject;
+    private int Students;
+    public Classroom (String Subject ,int  Students){
+        this. Subject = Subject ;
+        this. Students = Students;
+        }
+
+    public abstract String toString();
+    public String getSubject()
+    {
+        return Subject;
+    }
+    public void setSubject(String Subject)
+    {
+        this.Subject = Subject;
+    }
+    public int getStudents() {
+        return Students;
+    }
+    public void setStudents(int Students) {
+        this.Students = Students;
+    }
+
+
+    public boolean equals(Classroom classroom){
+        if(this.Subject.equals(classroom.getSubject())&& this.Students==(classroom.getStudents()))
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    public abstract void printClass();
 }
 
 public class (double GPA){
