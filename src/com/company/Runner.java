944668f9a9d906872bd253f9f3cd2d54b9893f16
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-10-27 19:08:18 -0400

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -10,8 +10,37 @@ public class Runner {
         {
             students[i] = randomStudent();
         }
-        Teacher teach = new Teacher("Folwell", "Nathan","Mr.", "CSA");
-        Classroom ourClass = new Classroom(students,teach);
+        Teacher teach = new Teacher("Folwell", "Nathan", "Mr.", "CSA") {
+            @Override
+            public String toString() {
+                return null;
+            }
+        };
+        Classroom ourClass = new Classroom(students, teach) {
+            @Override
+            public String toString() {
+                return null;
+            }
+
+            @Override
+            public void printClass() {
+
+            }
+        };
         ourClass.printClass();
     }
-}
+    public static Student randomStudent() {
+        String firstName = firstNames[(int) (Math.random() * firstNames.length)];
+        String familyName = familyNames[(int) (Math.random() * familyNames.length)];
+        Double GPA = Math.random() * 4.0;
+
+        Student stu = new Student(firstName, familyName, GPA) {
+            @Override
+            public String toString() {
+                return null;
+            }
+        };
+
+        return stu;
+    }
+    }
